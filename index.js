e63c61a6d620555725482061c34a3b4593de20ee
RitaSahakyan <rita.2002.sahakyan@gmail.com>
2025-03-07 19:59:56 +0400

    Changing

--- index.js
+++ index.js
@@ -7,12 +7,16 @@ const views = loadViews();
 app.get("/", (req, res) => {
   views["/"] += 1;
   saveViews(views);
-  res.send(`<h1>Главная страница</h1><p>Просмотров: ${views["/"]}</p>`);
+  res.send(
+    `<h1>Главная страница</h1><p>Просмотров: ${views["/"]}</p> <a href="/about">Перейти на страницу "О нас"</a>`
+  );
 });
 app.get("/about", (req, res) => {
   views["/about"] += 1;
   saveViews(views);
-  res.send(`<h1>О нас</h1><p>Просмотров: ${views["/about"]}</p>`);
+  res.send(
+    `<h1>О нас</h1><p>Просмотров: ${views["/about"]}</p> <a href="/">Перейти на главную страницу</a>`
+  );
 });
 
 app.listen(PORT, () => {
